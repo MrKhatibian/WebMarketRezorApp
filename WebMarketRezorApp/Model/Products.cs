c654dd9610ac13fb015ca2051905e07f98aa055a
MrKhatibian <mr.khatibian98@gmail.com>
2023-10-10 13:38:34 +0330

    Add Annotation for Product Model

--- WebMarketRezorApp/Model/Products.cs
+++ WebMarketRezorApp/Model/Products.cs
@@ -11,11 +11,28 @@ namespace WebMarketRezorApp.Model
         [Key]
         public int ID { get; set; }
 
+        [Required(ErrorMessage = "عنوان محصول را وارد نمایید")]
+        [Display(Name = "عنوان محصول")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "تصویر محصول را وارد نمایید")]
+        [Display(Name = "تصویر محصول")]
         public string Picture { get; set; }
+
+        [Required(ErrorMessage = "متن جایگزین تصویر محصول را وارد نمایید")]
+        [Display(Name = "متن جایگزین تصویر محصول")]
         public string PictureAlt { get; set; }
+
+        [Required(ErrorMessage = "عنوان تصویر محصول را وارد نمایید")]
+        [Display(Name = "عنوان تصویر محصول")]
         public string PictureTitle { get; set; }
+
+        [Required(ErrorMessage = "متن کوتاه محصول را وارد نمایید")]
+        [Display(Name = "متن کوتاه محصول")]
         public string ShortDiscription { get; set; }
+
+        [Required(ErrorMessage = "متن اصلی محصول را وارد نمایید")]
+        [Display(Name = "متن اصلی محصول")]
         public string Body { get; set; }
         public DateTime CreatDate { get; set; } = DateTime.Now;
 
