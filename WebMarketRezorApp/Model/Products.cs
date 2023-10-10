a227fc6aed144f6a90fe05efce1d4ab466b9889a
MrKhatibian <mr.khatibian98@gmail.com>
2023-10-10 13:27:35 +0330

    Make Product Model Property

---
+++ WebMarketRezorApp/Model/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMarketRezorApp.Model
+{
+    public class Products
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+        public string Picture { get; set; }
+        public string PictureAlt { get; set; }
+        public string PictureTitle { get; set; }
+        public string ShortDiscription { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatDate { get; set; } = DateTime.Now;
+
+    }
+}
