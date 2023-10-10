4a21a0a2ce0c9a42ace77db41ec4f424fb2afa70
MrKhatibian <mr.khatibian98@gmail.com>
2023-10-10 19:21:22 +0330

    Make Constractor and Property in Db

--- WebMarketRezorApp/Data/WebAppRazorDbContext.cs
+++ WebMarketRezorApp/Data/WebAppRazorDbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMarketRezorApp.Model;
 
 namespace WebMarketRezorApp.Data
 {
     public class WebAppRazorDbContext : DbContext
     {
+        public WebAppRazorDbContext(DbContextOptions<WebAppRazorDbContext> Option): base(Option)
+        {
+        }
+        //Make DbSet Products
+        public DbSet<Products> Products { get; set; }
     }
 }
