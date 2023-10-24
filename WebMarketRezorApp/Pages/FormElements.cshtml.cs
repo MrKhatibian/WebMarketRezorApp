e68f7b5ae74bbe592eddacbe504f2101a2d4a6ba
MrKhatibian <mr.khatibian98@gmail.com>
2023-10-24 22:52:00 +0330

    Add formsElemens Page

--- WebMarketRezorApp/Pages/FormElements.cshtml.cs
+++ WebMarketRezorApp/Pages/FormElements.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebMarketRezorApp.Pages.Admin
+namespace WebMarketRezorApp.Pages
 {
-    public class IndexModel : PageModel
+    public class FormElementsModel : PageModel
     {
         public void OnGet()
         {
