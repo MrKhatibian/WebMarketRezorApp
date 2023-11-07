42bcb4ae99654e15bc860fbe06deb68e69bcc687
MrKhatibian <mr.khatibian98@gmail.com>
2023-11-07 19:59:09 +0330

    Make Element prop for resive info from FormElements

--- WebMarketRezorApp/Pages/FormElements.cshtml.cs
+++ WebMarketRezorApp/Pages/FormElements.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebMarketRezorApp.Model;
 
 namespace WebMarketRezorApp.Pages
 {
     public class FormElementsModel : PageModel
     {
+        //Make Element prop for resive info from FormElements
+        public Elements Element { get; set; }
         public void OnGet()
         {
         }
