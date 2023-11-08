717669f7273ea3a0a130f608d944e03ce399283b
MrKhatibian <mr.khatibian98@gmail.com>
2023-11-08 12:40:26 +0330

    Creat obj for resive data from front in formElement

--- WebMarketRezorApp/Pages/FormElements.cshtml.cs
+++ WebMarketRezorApp/Pages/FormElements.cshtml.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebMarketRezorApp.Data;
 using WebMarketRezorApp.Model;
 
 namespace WebMarketRezorApp.Pages
 {
     public class FormElementsModel : PageModel
     {
+        // Creat obj for resive data from front
+        private readonly WebAppRazorDbContext _db;
+        // make constactor for access to Database
+        public FormElementsModel(WebAppRazorDbContext db)
+        {
+
+        }
         //Make Element prop for resive info from FormElements
         public Elements Element { get; set; }
-        public void OnGet()
+        
+        public void OnPost()
         {
         }
     }
