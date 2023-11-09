962caf3442504f73924dc95266fc89b9b27f02b7
MrKhatibian <mr.khatibian98@gmail.com>
2023-11-09 09:30:11 +0330

    Make DbContext object in ProfilePage

--- WebMarketRezorApp/Pages/Profile.cshtml.cs
+++ WebMarketRezorApp/Pages/Profile.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebMarketRezorApp.Data;
 
 namespace WebMarketRezorApp.Pages
 {
     public class ProfileModel : PageModel
     {
+        // Make DbContext object
+        private readonly WebAppRazorDbContext db;
         public void OnGet()
         {
         }
