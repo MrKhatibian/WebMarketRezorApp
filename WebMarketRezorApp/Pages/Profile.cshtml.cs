eb6284b8cdf55a59175799e2fbd1fb6574240cc7
MrKhatibian <mr.khatibian98@gmail.com>
2023-11-23 13:13:24 +0330

    Edit Profile View

--- WebMarketRezorApp/Pages/Profile.cshtml.cs
+++ WebMarketRezorApp/Pages/Profile.cshtml.cs
@@ -18,12 +18,28 @@ namespace WebMarketRezorApp.Pages
         {
             _db = db;
         }
+        [BindProperty]
         //Make Propert Element for Resive Element Talbe info
         public IEnumerable<Elements> Elements { get; set; }
         // Send Db info to Front
         public void OnGet()
         {
-            Elements = _db.Elements;
+            //Elements = _db.Elements.Find(id);
+            //Elements = _db.Elements;
+        }
+
+        // Make async post Method for Sent Data to db
+        public async Task<IActionResult> OnPost(Elements element)
+        {
+            //Add Validation for Server
+            if (ModelState.IsValid)
+            {
+                
+                _db.Elements.Update(element);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("/Profile");
+            }
+            return RedirectToPage("/Index");
         }
     }
 }
