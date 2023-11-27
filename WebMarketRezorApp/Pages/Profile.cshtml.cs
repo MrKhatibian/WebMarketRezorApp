35bb2ef048b33c46131013cb89fbe2567d54a92b
MrKhatibian <mr.khatibian98@gmail.com>
2023-11-27 13:15:35 +0330

    Edit Profile bachend for read data for my Id from DB

--- WebMarketRezorApp/Pages/Profile.cshtml.cs
+++ WebMarketRezorApp/Pages/Profile.cshtml.cs
@@ -11,46 +11,24 @@ namespace WebMarketRezorApp.Pages
 {
     public class ProfileModel : PageModel
     {
-        //// Make DbContext object
-        //private readonly WebAppRazorDbContext _db;
-        //// Make Constactor for Read DB info
-        //public ProfileModel(WebAppRazorDbContext db)
-        //{
-        //    _db = db;
-        //}
-        //[BindProperty]
-        ////Make Propert Element for Resive Element Talbe info
-        ////public IEnumerable<Elements> Element { get; set; }
-        //public Elements Element { get; set; }
-        //// Send Db info to Front
-        ////public void OnGet()
-        ////{
-        ////    //Elements = _db.Elements.Find(id);
-        ////    Element = _db.Elements;
-        ////}
-
-        //// Make async post Method for Sent Data to db
-        //public async Task<IActionResult> OnPost(Elements Element)
-        //{
-        //    //Add Validation for Server
-        //    if (ModelState.IsValid)
-        //    {                
-        //        await _db.Elements.AddAsync(Element);
-        //        await _db.SaveChangesAsync();
-        //        return RedirectToPage("/Profile");
-        //    }
-        //    return RedirectToPage("/Index");
-        //}
-
-        // Creat obj for resive data from front
+        // Make DbContext object
         private readonly WebAppRazorDbContext _db;
-        // make constactor for access to Database
+        // Make Constactor for Read DB info
         public ProfileModel(WebAppRazorDbContext db)
         {
             _db = db;
         }
-        //Make Element prop for resive info from FormElements
+        [BindProperty]
+        //Make Propert Element for Resive Element Talbe info
+        public IEnumerable<Elements> IE_Element { get; set; }
         public Elements Element { get; set; }
+        // Send Db info to Front
+        public void OnGet()
+        {
+            Element = _db.Elements.Find(1002);
+            //IE_Element = _db.Elements;
+        }
+
         // Make async post Method for Sent Data to db
         public async Task<IActionResult> OnPost(Elements Element)
         {
@@ -59,9 +37,9 @@ namespace WebMarketRezorApp.Pages
             {
                 await _db.Elements.AddAsync(Element);
                 await _db.SaveChangesAsync();
-                return RedirectToPage("/Index");
+                return RedirectToPage("/Profile");
             }
-            return Page();
+            return RedirectToPage("/Index");
         }
     }
 }
