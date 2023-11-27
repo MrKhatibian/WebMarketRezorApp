53da2866ddac15b8a72f162b2fa8180126ca76ed
MrKhatibian <mr.khatibian98@gmail.com>
2023-11-27 13:38:17 +0330

    Make UserMangment Backend

--- WebMarketRezorApp/Pages/UserMangment.cshtml.cs
+++ WebMarketRezorApp/Pages/UserMangment.cshtml.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebMarketRezorApp.Data;
+using WebMarketRezorApp.Model;
 
 namespace WebMarketRezorApp.Pages
 {
     public class UserMangmentModel : PageModel
     {
+        // Make DbContext object
+        private readonly WebAppRazorDbContext _db;
+        // Make Constactor for Read DB info
+        public UserMangmentModel(WebAppRazorDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        //Make Propert Element for Resive Element Talbe info
+        public IEnumerable<Elements> IE_Element { get; set; }
+        public Elements Element { get; set; }
+        // Send Db info to Front
         public void OnGet()
         {
-        }
+            Element = _db.Elements.Find(1002);
+            //IE_Element = _db.Elements;
+        }        
     }
 }
